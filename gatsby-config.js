130349d347a40f7ecf02590444d04db69101abb0
dhilipkumarV <dhilip.velusamy@go-mmt.com>
2019-11-04 23:02:09 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,21 +4,25 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content/posts/`,
+      }
+    },
+    'gatsby-transformer-remark'
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Adventures of Namma Pasanga`,
+    author: `Adventures of Namma Pasanga`,
+    description: `A Blog where Namma Pasanga records their various Adventures`,
     social: [
       {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/gatsbyjs`,
-      },
+        name: ``,
+        url: ``,
+      }
     ],
   },
 }
